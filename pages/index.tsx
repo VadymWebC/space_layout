3cdd9637a16de4b9cb7a1bb39adab2a2a76839c3
VadymWebC <vadymcdevelweb@gmail.com>
2023-06-07 22:20:14 +0300

    add logo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,37 @@
 import styles from '../styles/home.module.scss'
 import cl from 'classnames'
 
+import { Logo } from '../components/Logo'
+
 export default function Home() {
     return (
         <div>
             <section className={cl(styles.section, styles.sectionMain)}>
-                <header></header>
+                <header>
+                    <Logo />
+                    {/*
+                        <SocialNetworks />
+                    <Button onClick={() => {}}>Whitepaper</Button>
+*/}
+                </header>
+
+                {/*    <Navigation /> */}
+
+                {/*
+                    <div>
+                        <Title type="h2">From dusk to dawn</Title>
+                        <ul>
+                            <li>
+                                <Button onClick={() => {}}>Mint</Button>
+                            </li>
+                            <li>
+                                <Button onClick={() => {}}>
+                                    Connect Wallet
+                                </Button>
+                            </li>
+                        </ul>
+                    </div>
+                    */}
             </section>
         </div>
     )
