983910f0375704bec2d960a98e7b22e43271e283
VadymWebC <vadymcdevelweb@gmail.com>
2023-06-10 20:54:06 +0300

    Button component development

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import styles from './style.module.scss'
 
-interface ButtonProps {}
-
-export const Button: React.FC<ButtonProps> = () => {
-    return (
-        <ul>
-            <li></li>
-        </ul>
-    )
+interface ButtonProps {
+    onClick: () => void
+    children: ReactNode
 }
+
+export const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+    return <button onClick={onClick}>{children}</button>
+}
+
+// interface ButtonProps {}
+
+// export const Button: React.FC<ButtonProps> = () => {
+//     return (
+//         <div>
+//             <span></span>
+//         </div>
+//     )
+// }
