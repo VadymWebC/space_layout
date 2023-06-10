4047b1000cfa7bcb543b7b80d2a20fdca243bd66
VadymWebC <vadymcdevelweb@gmail.com>
2023-06-10 20:55:18 +0300

    Navigation component development

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -5,9 +5,5 @@ import styles from './style.module.scss'
 interface NavigationProps {}
 
 export const Navigation: React.FC<NavigationProps> = () => {
-    return (
-        <ul>
-            <li></li>
-        </ul>
-    )
+    return <div>Navigation</div>
 }
