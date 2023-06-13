6994c5105f32b54fa008b2346b1f5ffff31ebc28
VadymWebC <vadymcdevelweb@gmail.com>
2023-06-13 21:27:10 +0300

    Button development

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -21,6 +21,22 @@ export const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
                     d="M4,8.83s1.23,0,12.22,0L4,20.54s0-11.71,0-11.71Z"
                 />
             </svg>
+
+            <svg viewBox="0 0 31.96 56" className={styles.buttonBorderCenter}>
+                <path className={styles.buttonLine} d="M0,6H31.96" />
+                <path className={styles.buttonArrow} d="M-.07,55H32.06" />
+            </svg>
+
+            <svg viewBox="0 0 36.12 56.5" className={styles.buttonBorderRight}>
+                <path
+                    className={styles.buttonLine}
+                    d="M0,6.5H1.92L10.42,1h24.71V55.5H0"
+                />
+                <path
+                    className={styles.buttonArrow}
+                    d="M31.46,52.21s-1.23,0-12.22,0l12.22-11.71s0,11.71,0,11.71Z"
+                />
+            </svg>
         </button>
     )
 }
