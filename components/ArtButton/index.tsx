a6c1bd52090b8bd91467a2197d1c332dc7766ada
VadymWebC <vadymcdevelweb@gmail.com>
2023-06-16 18:25:40 +0300

    fix

--- components/ArtButton/index.tsx
+++ components/ArtButton/index.tsx
@@ -2,15 +2,21 @@ import Link from 'next/link'
 import { ReactNode } from 'react'
 
 import styles from './style.module.scss'
+import cl from 'classnames'
 
 interface ArtButtonProps {
     onClick: () => void
     children: ReactNode
+    className: string
 }
 
-export const ArtButton: React.FC<ArtButtonProps> = ({ onClick, children }) => {
+export const ArtButton: React.FC<ArtButtonProps> = ({
+    onClick,
+    children,
+    className,
+}) => {
     return (
-        <button className={styles.artButton} onClick={onClick}>
+        <button className={cl(styles.artButton, className)} onClick={onClick}>
             <span className={styles.artButtonText}>{children}</span>
             <span className={styles.artButtonAngleLeft} />
             <span className={styles.artButtonAngleRight} />
