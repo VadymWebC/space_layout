718df99ba44b1a7c36c564e2ab27462d4a85664f
VadymWebC <vadymcdevelweb@gmail.com>
2023-06-17 22:03:15 +0300

    Art Button

--- components/ArtButton/index.tsx
+++ components/ArtButton/index.tsx
@@ -3,20 +3,31 @@ import { ReactNode } from 'react'
 
 import styles from './style.module.scss'
 import cl from 'classnames'
+export type TypeArtButton = 'BluePurple' | 'GreenYellow' | 'Blue' | 'YellowPink'
 
 interface ArtButtonProps {
     onClick: () => void
     children: ReactNode
     className: string
+    type?: TypeArtButton
 }
 
 export const ArtButton: React.FC<ArtButtonProps> = ({
     onClick,
     children,
     className,
+    type,
 }) => {
     return (
-        <button className={cl(styles.artButton, className)} onClick={onClick}>
+        <button
+            className={cl(
+                styles.artButton,
+                className,
+                type && styles.artButtonType,
+                type && styles[`artButton${type}`]
+            )}
+            onClick={onClick}
+        >
             <span className={styles.artButtonText}>{children}</span>
             <span className={styles.artButtonAngleLeft} />
             <span className={styles.artButtonAngleRight} />
